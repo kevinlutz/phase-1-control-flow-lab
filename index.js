21ac18a908e88179c70cd974a42e4c737fa324df
+ <+>
2021-09-04 21:08:39 -0400

    commit tree 7cbbd59b6292fa36facc16c65412c9107ea0de88

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+//what about distances between 400-2000
+//what about distances between 2000-2500
+//THIS IS FIZZBUZZ
+//FiZZBUZZ can be written backwards if switch statement is used. (but double check!)
+
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet <= 400) {
+    result = "This one is on me!";
+  } else if (feet > 2500) {
+    result = "No can do.";
+  } else if (feet > 2000) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  //const tip = "generous";
+  let response;
+
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much.";
+      break;
+    case "not as generous":
+      response = "Thank you.";
+      break;
+    case "thanks for everything":
+      response = "Bye.";
+      break;
+  }
+  return response;
 }
